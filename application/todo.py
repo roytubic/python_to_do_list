07700c73b65e5a19f317a39194692db7d31449a6
Roy-Tubic <roytubic@gmail.com>
2023-10-26 17:16:30 +1300

    initial set up of main*

---
+++ application/todo.py
@@ -0,0 +1,15 @@
+from tkinter import *
+from tkinter import ttk
+
+class todo:
+    def __init__(self,  root):
+        pass
+
+
+def main():
+    root = Tk()
+    ui = todo(root)
+    root.mainloop()
+
+if __name__ == "__main__":
+    main()
